b85683269a45ed8d72014758ccb5b5f4beab4391
John David Dela Cruz <johndaviddc1@gmail.com>
2024-02-15 12:23:55 +0800

    crypto service api

---
+++ src/test/java/dave/dev/cryptoservice/CryptoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.cryptoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
